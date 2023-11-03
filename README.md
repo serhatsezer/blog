5f1b2ca164de46aa82fb89ab2a1f5fa0e20d7b98
serhatsezer <serhat.sezer@reaktor.com>
2023-11-03 15:32:03 +0200

    Initial commit for blog website

---
+++ README.md
@@ -0,0 +1,3 @@
+# Personal Blog
+
+This is my side project for my personal blog. Reason for me to do this is learn and practice astro.build.
---
+++ src/config.ts
@@ -0,0 +1,12 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Serhat Sezer";
+export const SITE_DESCRIPTION =
+  "Welcome to my blog! I write about modern software development";
+export const TWITTER_HANDLE = "@serhatsezer";
+export const MY_NAME = "Serhat Sezer";
+
+// setup in astro.config.mjs
+const BASE_URL = new URL(import.meta.env.SITE);
+export const SITE_URL = BASE_URL.origin;
